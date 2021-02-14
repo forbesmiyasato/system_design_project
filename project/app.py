8def556c5677f0facfe041c5af660a765c7393b2
Ubuntu <ubuntu@ip-172-31-55-37.ec2.internal>
2021-02-14 00:15:53 +0000

    intergrating sqs

--- project/app.py
+++ project/app.py
@@ -1,11 +1,21 @@
+import boto3
 from flask import Flask
 
 app = Flask(__name__)
-
+sqs = boto3.resource('sqs')
+queue = sqs.get_queue_by_name(QueueName='tasks.fifo')
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+@app.route('/transform', methods=['POST'])
+def transform():
+    from_format = request.args.get('from_format')
+    to_format = request.args.get('to_format')
+    filename = request.args.get('filename')
+    print(queue.url)
 
 
 if __name__ == '__main__':
