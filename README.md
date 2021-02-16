bdedb03b522094c1251ccfe190eb1cf028b1bc8a
forbesmiyasato <miya5288@pacificu.edu>
2021-02-15 20:50:01 -0800

    updated readme and cleaned some code

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-sudo apt install nginx, python3-venv, python3-dev
+sudo apt install nginx python3-venv python3-dev awscli
+
+aws config #aws credentials and configurations
+
 create file /etc/nginx/site-enabled/app
+
 sudo service nginx restart
 
 In project director
+
 python3 -m venv env
+
 source env/bin/activate
+
 pip install -r requirements.txt
+
 cd project
+
 gunicorn app:app
 
 access at http://52.27.158.127/80
--- project/worker.py
+++ project/worker.py
@@ -2,7 +2,6 @@ import boto3
 import json
 import csv
 import io
-import pandas as pd
 
 bucket_name = "aws-system-design"
 sqs = boto3.resource('sqs')
@@ -10,7 +9,28 @@ s3 = boto3.client('s3')
 
 queue = sqs.get_queue_by_name(QueueName='tasks.fifo')
 
-def handleTask():
+def get_json(content):
+    csv_reader = csv.DictReader(io.StringIO(content))
+    json_data = json.dumps(list(csv_reader))
+    return str(json.dumps(json_data))
+
+
+def get_csv(content):
+    json_data = json.loads(content) # first load reformats the json data into proper string format
+    json_data = json.loads(json_data)
+    in_memory_file = io.StringIO()
+    csv_writer = csv.writer(in_memory_file)
+    header = True
+    for row in json_data:
+        if header:
+            header = row.keys()
+            csv_writer.writerow(header)
+            header = False
+        csv_writer.writerow(row.values())
+    return in_memory_file.getvalue()
+
+
+def handle_task():
     while True:
         messages = queue.receive_messages()
         if messages:
@@ -22,48 +42,38 @@ def handleTask():
             to_format = data['to_format']
             filename = data['filename']
             object_key = filename + '.' + from_format
+            csv_to_json = from_format == 'csv' and to_format == 'json'
+            json_to_csv = from_format == 'json' and to_format == 'csv'
+
+            if not csv_to_json or not json_to_csv:
+                # invalid message
+                message.delete()
+                continue
 
             try:
                 file_obj = s3.get_object(Bucket=bucket_name, Key=object_key)
             except:
-                #handle exception from getting file
-                pass
+                # handle exception from getting file
+                continue
 
             file_body = file_obj['Body']
             content = file_body.read().decode('utf-8-sig')
             body = None
-            valid_message = True
 
-            if from_format == 'csv' and to_format == 'json':
-                csv_reader = csv.DictReader(io.StringIO(content))
-                json_data = json.dumps(list(csv_reader))
-                body = str(json.dumps(json_data))
-            elif from_format == 'json' and to_format == 'csv':
-                json_data = json.loads(content) #first load reformats the json data into proper string format
-                json_data = json.loads(json_data)
-                in_memory_file = io.StringIO()
-                csv_writer = csv.writer(in_memory_file)
-                header = True
-                for row in json_data:
-                    if header:
-                        header = row.keys()
-                        csv_writer.writerow(header)
-                        header = False
-                    csv_writer.writerow(row.values())
-                body = in_memory_file.getvalue()
-            else:
-                valid_message = False
+            if csv_to_json:
+                body = get_json(content)
+            elif json_to_csv:
+                body = get_csv(content)
 
-            if valid_message:
-                new_object_key = filename + '.' + to_format
-                try:
-                    s3.put_object(Body=body, Bucket=bucket_name, Key=new_object_key)
-                except:
-                    continue
-                s3.delete_object(Bucket=bucket_name, Key=object_key)
-
-            message.delete() #delete message if valid or invalid?
+            new_object_key = filename + '.' + to_format
+            try:
+                s3.put_object(Body=body, Bucket=bucket_name, Key=new_object_key)
+            except:
+                # handle exception from uploading file
+                continue
+            s3.delete_object(Bucket=bucket_name, Key=object_key)
+            message.delete()
 
 if __name__ == '__main__':
     print("Worker running")
-    handleTask()
+    handle_task()
