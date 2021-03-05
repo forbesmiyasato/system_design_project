fb15ae787c520091408c6d019638425aae4453fd
forbesmiyasato <miya5288@pacificu.edu>
2021-03-04 16:00:32 -0800

    multiprocessing for workers

--- project/part_two/worker.py
+++ project/part_two/worker.py
@@ -4,6 +4,7 @@ import csv
 import io
 import requests
 import time
+from multiprocessing import Process
 
 import message_queue as queue
 
@@ -34,75 +35,72 @@ def convert_from_json_to_csv(content):
     return in_memory_file.getvalue()
 
 
-def handle_task():
+def handle_task(message):
+    time.sleep(10)  # pretend like it takes longer to receive a message
+    request_id = message["request_id"]
+    print(f"received request {request_id}")
+    url = "http://52.27.158.127:80/update"
+    requests.post(
+        url,
+        data={
+            "request_id": request_id,
+            "status": "Processing request",
+            "code": "2",
+        },
+    )
+    from_format = message["from_format"]
+    to_format = message["to_format"]
+    object_name = message["key"]
+    bucket_name = message["bucket"]
+    object_key = object_name + "." + from_format
+    csv_to_json = from_format == "csv" and to_format == "json"
+    json_to_csv = from_format == "json" and to_format == "csv"
+
+    if not csv_to_json and not json_to_csv:
+        # invalid message
+        print("invalid message format")
+        message.delete()
+    try:
+        file_obj = s3.get_object(Bucket=bucket_name, Key=object_key)
+    except Exception:
+        # TODO handle exception from getting file
+        pass
+
+    file_body = file_obj["Body"]
+    content = file_body.read().decode("utf-8-sig")
+    body = None
+
+    if csv_to_json:
+        body = convert_from_csv_to_json(content)
+    elif json_to_csv:
+        body = convert_from_json_to_csv(content)
+
+    time.sleep(10)  # pretend like it takes longer for task to process
+    new_object_key = object_name + "." + to_format
+    try:
+        s3.put_object(Body=body, Bucket=bucket_name, Key=new_object_key)
+    except Exception:
+        # TODO handle exception from uploading file
+        pass
+    s3.delete_object(Bucket=bucket_name, Key=object_key)
+    message.delete()
+    requests.post(
+        url,
+        data={
+            "request_id": request_id,
+            "status": "Conversion completed",
+            "code": "3",
+        },
+    )
+    print(f"finished request {request_id}")
+
+
+if __name__ == "__main__":
+    print("Worker running")
     # Receives messages from the queue and processes the conversions.
     while True:
         message = queue.receive_messages()
         print(message)
         if message:
-            time.sleep(10)  # pretend like it takes longer to receive a message
-            request_id = message["request_id"]
-            print(f"received request {request_id}")
-            url = "http://52.27.158.127:80/update"
-            requests.post(
-                url,
-                data={
-                    "request_id": request_id,
-                    "status": "Processing request",
-                    "code": "2",
-                },
-            )
-            from_format = message["from_format"]
-            to_format = message["to_format"]
-            object_name = message["key"]
-            bucket_name = message["bucket"]
-            object_key = object_name + "." + from_format
-            csv_to_json = from_format == "csv" and to_format == "json"
-            json_to_csv = from_format == "json" and to_format == "csv"
-
-            if not csv_to_json and not json_to_csv:
-                # invalid message
-                print("invalid message format")
-                message.delete()
-                continue
-
-            try:
-                file_obj = s3.get_object(Bucket=bucket_name, Key=object_key)
-            except Exception:
-                # handle exception from getting file
-                continue
-
-            file_body = file_obj["Body"]
-            content = file_body.read().decode("utf-8-sig")
-            body = None
-
-            if csv_to_json:
-                body = convert_from_csv_to_json(content)
-            elif json_to_csv:
-                body = convert_from_json_to_csv(content)
-
-            time.sleep(10)  # pretend like it takes longer for task to process
-            new_object_key = object_name + "." + to_format
-            try:
-                s3.put_object(
-                    Body=body, Bucket=bucket_name, Key=new_object_key
-                )
-            except Exception:
-                # handle exception from uploading file
-                continue
-            s3.delete_object(Bucket=bucket_name, Key=object_key)
-            message.delete()
-            requests.post(
-                url,
-                data={
-                    "request_id": request_id,
-                    "status": "Conversion completed",
-                    "code": "3",
-                },
-            )
-            print(f"finished request {request_id}")
-
-
-if __name__ == "__main__":
-    print("Worker running")
-    handle_task()
+            p = Process(target=handle_task, args=(message,))
+            p.start()
