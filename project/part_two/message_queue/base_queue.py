027db23eb81e3bcb1f32acb5a929197d006fcc74
forbesmiyasato <miya5288@pacificu.edu>
2021-03-14 01:19:14 -0800

    comments

--- project/part_two/message_queue/base_queue.py
+++ project/part_two/message_queue/base_queue.py
@@ -1,14 +1,29 @@
 class MessageQueue:
+    """
+    A class used as an abstraction of queue operations
+
+    Methods
+    -------
+    send_message(message)
+        Sends a message to the queue
+
+    receive_message()
+        Get a message from the queue
+    """
+
     def send_message(self, message):
-        print("parent")
-        """
-        Delivers a message to the specified queue.
-        Message format: {
-            "from_format": ,
-            "to_format": ,
-            "key": ,
-            "bucket": ,
-        }
+        """Sends a message to the queue
+
+        Parameters
+        ----------
+        message : int
+            The id of the request record we want to retrive from the DB
+
+        Returns
+        ------
+        True (has error) and error message if can't retrieve the
+        request from DB, and False (has no error) and request json if retrieved
+        request from DB
         """
         if type(message) is not dict:
             raise ValueError("Invalid type for message, expecting type 'dict'")
